f12de959d47c23c20b681234a36063e53c79f26d
hector4213 <hectorpwns@gmail.com>
2021-04-15 01:20:34 -0400

    new to room controller with changeset

--- lib/chatty_web/controllers/room_controller.ex
+++ lib/chatty_web/controllers/room_controller.ex
@@ -1,8 +1,14 @@
 defmodule ChattyWeb.RoomController do
   use ChattyWeb, :controller
 
+  alias Chatty.Talk.Room
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
+  def new(conn, _params) do
+    changeset = Room.changeset(%Room{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
 end
