a30435c64b12cd802a020692c2882a44e986fc84
hector4213 <hectorpwns@gmail.com>
2021-04-16 00:38:05 -0400

    functions to update room

--- lib/chatty/talk.ex
+++ lib/chatty/talk.ex
@@ -6,10 +6,20 @@ defmodule Chatty.Talk do
     Repo.all(Room)
   end
 
+  def change_room(%Room{} = room) do
+    Room.changeset(room, %{})
+  end
+
   def create_room(attrs \\ %{}) do
     %Room{}
     |> Room.changeset(attrs)
     |> Repo.insert()
+  end
+
+  def update_room(%Room{} = room, attrs) do
+    room
+    |> Room.changeset(attrs)
+    |> Repo.update()
   end
 
   def get_room!(id), do: Repo.get!(Room, id)
