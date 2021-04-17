e7f4e1a513b73864cddfd9fb97bd6415ab6a3ccb
hector4213 <hectorpwns@gmail.com>
2021-04-17 08:13:42 -0400

    function for create session, use db interface with alias

--- lib/chatty_web/controllers/session_controller.ex
+++ lib/chatty_web/controllers/session_controller.ex
@@ -1,7 +1,23 @@
 defmodule ChattyWeb.SessionController do
   use ChattyWeb, :controller
+  alias Chatty.Accounts
 
   def new(conn, _) do
     render(conn, "new.html")
+  end
+
+  def create(conn, %{"session" => %{"email" => email, "password" => password}}) do
+    case Accounts.sign_in(email, password) do
+      {:ok, user} ->
+        conn
+        |> put_session(:current_user_id, user.id)
+        |> put_flash(:info, "You are signed in")
+        |> redirect(to: Routes.room_path(conn, :index))
+
+      {:error, _} ->
+        conn
+        |> put_flash(:error, "Invalid Email or Password")
+        |> render("new.html")
+    end
   end
 end
