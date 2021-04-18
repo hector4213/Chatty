25a0514c468d0b2e1e2c3c7b00dbdea91f7d76ae
hector4213 <hectorpwns@gmail.com>
2021-04-17 22:33:44 -0400

    use virtual fields

--- lib/chatty/accounts/user.ex
+++ lib/chatty/accounts/user.ex
@@ -7,7 +7,8 @@ defmodule Chatty.Accounts.User do
     field :email, :string
     field :password_hash, :string
     field :username, :string
-
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
     timestamps()
   end
 
