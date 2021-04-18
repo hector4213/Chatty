7be4b535bcf7bdd3f1a9783f9dccb3cb915e7118
hector4213 <hectorpwns@gmail.com>
2021-04-17 22:34:29 -0400

    do create function to register new user

--- lib/chatty_web/controllers/registration_controller.ex
+++ lib/chatty_web/controllers/registration_controller.ex
@@ -1,14 +1,22 @@
 defmodule ChattyWeb.RegistrationController do
   use ChattyWeb, :controller
 
+  alias Chatty.Accounts
+
   def new(conn, _) do
     render(conn, "new.html", changeset: conn)
   end
 
-  def create(conn, params) do
-    IO.puts("this is params")
-    inspect(params)
-    IO.puts("this is conn")
-    conn
+  def create(conn, %{"registration" => registration_params}) do
+    case Accounts.register(registration_params) do
+      {:ok, user} ->
+        conn
+        |> put_session(:current_user_id, user.id)
+        |> put_flash(:info, "You have Signed in")
+        |> redirect(to: Routes.room_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
