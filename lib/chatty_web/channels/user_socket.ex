04e4b87fe4341939b963dcfc9936b0d399ae728d
hector4213 <hectorpwns@gmail.com>
2021-04-19 00:57:56 -0400

    user to use room channel

--- lib/chatty_web/channels/user_socket.ex
+++ lib/chatty_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ChattyWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", ChattyWeb.RoomChannel
+  channel "room:*", ChattyWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
