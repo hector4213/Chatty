c96d952aa3c531b341537f03504d69b4381abb5e
hector4213 <hectorpwns@gmail.com>
2021-04-19 00:56:49 -0400

    use js / websockets

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -6,9 +6,9 @@
 //
 // Pass the token on params as below. Or remove it
 // from the params if you are not using authentication.
-import {Socket} from "phoenix"
+import { Socket } from 'phoenix'
 
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+let socket = new Socket('/socket', { params: { token: window.userToken } })
 
 // When you connect, you'll often need to authenticate the client.
 // For example, imagine you have an authentication plug, `MyAuth`,
@@ -55,9 +55,31 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+let channel = socket.channel('room:general', {})
+channel
+	.join()
+	.receive('ok', (resp) => {
+		console.log('Joined successfully', resp)
+	})
+	.receive('error', (resp) => {
+		console.log('Unable to join', resp)
+	})
+
+channel.on('room:general:new_message', (message) => {
+	displayMessage(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+	e.preventDefault()
+	let input = e.target.querySelector('#message-body')
+	channel.push('message:add', { message: input.value })
+	input.value = ''
+})
+
+const displayMessage = (msg) => {
+	let template = `<li>${msg.body}</li>`
+
+	document.querySelector('#display').innerHTML += template
+}
 
 export default socket
