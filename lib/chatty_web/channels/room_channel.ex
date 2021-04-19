6b75ab56b153815c95f401ed515f2af3cb32f4b7
hector4213 <hectorpwns@gmail.com>
2021-04-19 00:57:10 -0400

    create room channel

---
+++ lib/chatty_web/channels/room_channel.ex
@@ -0,0 +1,12 @@
+defmodule ChattyWeb.RoomChannel do
+  use ChattyWeb, :channel
+
+  def join(channel_name, _params, socket) do
+    {:ok, %{channel: channel_name}, socket}
+  end
+
+  def handle_in("message:add", %{"message" => body}, socket) do
+    broadcast!(socket, "room:general:new_message", %{"body" => body})
+    {:reply, :ok, socket}
+  end
+end
