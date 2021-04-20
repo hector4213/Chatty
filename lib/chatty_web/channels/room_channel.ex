90b9148d19633b959e8650777dc522b11dd77c46
hector4213 <hectorpwns@gmail.com>
2021-04-19 23:52:06 -0400

    Add presence to room channel, keeps track of user status/info

--- lib/chatty_web/channels/room_channel.ex
+++ lib/chatty_web/channels/room_channel.ex
@@ -3,8 +3,10 @@ defmodule ChattyWeb.RoomChannel do
 
   alias Chatty.Repo
   alias Chatty.Accounts.User
+  alias ChattyWeb.Presence
 
   def join("room:" <> room_id, _params, socket) do
+    send(self(), :after_join)
     {:ok, %{channel: "room:#{room_id}"}, assign(socket, :room_id, room_id)}
   end
 
@@ -14,6 +16,16 @@ defmodule ChattyWeb.RoomChannel do
     message = %{body: body, user: %{username: user.username}}
     broadcast!(socket, "room:#{room_id}:new_message", message)
     {:reply, :ok, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    push(socket, "presence_state", Presence.list(socket))
+    user = get_user(socket)
+
+    {:ok, _} =
+      Presence.track(socket, "user:#{user.id}", %{user_id: user.id, username: user.username})
+
+    {:no_reply, socket}
   end
 
   def get_user(socket) do
